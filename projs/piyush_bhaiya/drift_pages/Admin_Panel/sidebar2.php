ded0c76b592cecc2ee136b450a8487dff728feb9
BrahmDevPandey <brahmdevpandey762@gmail.com>
2022-11-19 12:10:04 +0530

    driftias admin panel complete

---
+++ projs/piyush_bhaiya/drift_pages/Admin_Panel/sidebar2.php
@@ -0,0 +1,48 @@
+<!-- Sidebar -->
+<ul class="navbar-nav  sidebar sidebar-dark accordion" style="background-color: blue;" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+        </div>
+        <div class="sidebar-brand-text mx-3">Medhaj Astro </div>
+    </a>
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="registraion.php">
+        <i class="fa fa-user-plus" aria-hidden="true"></i>               
+            <span>Registration</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+    <li class="nav-item">
+        <a class="nav-link" href="enquiey.php">
+        <i class="fas fa-comments" aria-hidden="true"></i>               
+            <span>Enquiry</span></a>
+    </li>
+    <hr class="sidebar-divider d-none d-md-block">
+    <li class="nav-item">
+        <a class="nav-link" href="registraion.php">
+        <i class="fa fa-user" aria-hidden="true"></i>               
+            <span>Users</span></a>
+    </li>
+   
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle" style="background-color: white;"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>
+    </div>
+
+  
+
+</ul>
+<!-- End of Sidebar -->
